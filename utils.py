52a9e4234c4b95578f9f71e7438646f81afc0872
aashavskiy <ashavskiy@gmail.com>
2025-01-30 15:34:13 +0200

    1

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 
 WHITELIST_FILE = "whitelist.txt"
+VPN_WHITELIST_FILE = "vpn_whitelist.txt"
 
 def load_whitelist(filename=WHITELIST_FILE):
     """ Load the whitelist from a file. """
@@ -9,6 +10,14 @@ def load_whitelist(filename=WHITELIST_FILE):
     
     with open(filename, "r") as file:
         return set(line.strip().split()[0] for line in file if line.strip())
+
+def add_to_whitelist(user_id, username, filename=WHITELIST_FILE):
+    """ Add a user to the whitelist. """
+    if not os.path.exists(filename):
+        open(filename, "w").close()  # Create file if missing
+    
+    with open(filename, "a") as file:
+        file.write(f"{user_id} # {username}\n")
 
 def check_whitelist(user_id):
     """ Check if a user is in the whitelist. """
