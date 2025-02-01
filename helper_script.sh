70da63b4d81d730581f3e8a31392e7edb285b957
aashavskiy <ashavskiy@gmail.com>
2025-02-01 19:11:25 +0200

    Remove redundant configuration validation in helper script

--- helper_script.sh
+++ helper_script.sh
@@ -7,15 +7,6 @@ PUBLIC_KEY=$4
 DEVICE_IP=$5
 WG_CONFIG_FILE="/etc/wireguard/wg0.conf"
 
-validate_config() {
-    wg setconf wg0 $WG_CONFIG_FILE 2>&1 | tee /tmp/wg_checkconf_output.log
-    if [ $? -ne 0 ]; then
-        echo "Invalid WireGuard configuration. Aborting."
-        cat /tmp/wg_checkconf_output.log
-        exit 1
-    fi
-}
-
 validate_ip() {
     if [[ ! $DEVICE_IP =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
         echo "Invalid IP address format. Aborting."
@@ -26,10 +17,8 @@ validate_ip() {
 if [ "$ACTION" == "add" ]; then
     validate_ip
     echo -e "\n# ${USERNAME}_${DEVICE_NAME}\n[Peer]\nPublicKey = ${PUBLIC_KEY}\nAllowedIPs = ${DEVICE_IP}/32" >> $WG_CONFIG_FILE
-    validate_config
 elif [ "$ACTION" == "remove" ]; then
     sed -i "/# ${USERNAME}_${DEVICE_NAME}/,/^$/d" $WG_CONFIG_FILE
-    validate_config
 fi
 
 # Restart WireGuard
