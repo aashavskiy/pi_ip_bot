218c7bda669007a548d118abd28632b75127b256
aashavskiy <ashavskiy@gmail.com>
2025-02-16 14:31:03 +0200

    1

--- commands/buttons.py
+++ commands/buttons.py
@@ -1,6 +1,6 @@
 # commands/buttons.py
 
-from commands.vpn.config import vpn_menu  # Importing vpn_menu from config.py
+from commands.vpn.menu import vpn_menu  # Importing vpn_menu from menu.py
 
 def handle_menu_buttons():
     print(vpn_menu())  # Calling vpn_menu function
