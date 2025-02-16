2fe539034e948b28d509c568bba0345f07fec4d3
aashavskiy <ashavskiy@gmail.com>
2025-02-16 14:42:04 +0200

    1

--- commands/menu.py
+++ commands/menu.py
@@ -1,6 +1,12 @@
 # /Users/alexanderashavskiy/projects/pi_ip_bot/menu.py
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher.filters import Command
+from aiogram.types import Message
+from aiogram.dispatcher import Dispatcher
+
+# Get the current dispatcher instance
+dp = Dispatcher.get_current()
 
 # Define main menu buttons
 main_menu = ReplyKeyboardMarkup(
@@ -11,3 +17,16 @@ main_menu = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
+
+# Function to return the main menu markup
+def get_main_menu():
+    return main_menu
+
+# Command handler for opening the menu
+@dp.message_handler(Command("menu"))
+async def menu_command(message: Message):
+    await message.reply("Main menu:", reply_markup=get_main_menu())
+
+# Placeholder for conversation handler (if needed)
+def get_conversation_handler():
+    pass  # Implement if needed
