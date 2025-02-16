3a2eb3341c6cdda5d43ebb96c4a96f058d0e1465
aashavskiy <ashavskiy@gmail.com>
2025-02-16 14:47:34 +0200

    Fixed incorrect import path in `commands/vpn/__init__.py`

--- commands/vpn/__init__.py
+++ commands/vpn/__init__.py
@@ -1,5 +1,5 @@
 # /Users/alexanderashavskiy/projects/pi_ip_bot/commands/vpn/__init__.py
 
-from .vpn import vpn_button_handler, router
+from . import vpn_button_handler, router
 
 __all__ = ["vpn_button_handler", "router"]
